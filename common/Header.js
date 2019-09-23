0c7de7fb0f56d59a06b1cfb907aecaa2e0f5fc7d
levon <levonhovsepyan1992@gmail.com>
2019-09-23 20:55:36 +0400

    header,loading,logo

---
+++ common/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import logo from './logo.png';
+import "./Header.css";
+const Header = () => {
+    return(
+        <div className="Header">
+        <Link to="/list">
+            <img src={logo} alt="logo" className="Header-logo"/>
+        </Link>
+        </div>
+    )
+}
+
+export default Header;
